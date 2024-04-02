f41eb425ed2ea86ecae9d51b5a464ff2ce5d5168
Elaine Chan <elaine.chan@itv.com>
2024-04-02 12:51:37 +0100

    refactor

--- databricks_utility/__main__.py
+++ databricks_utility/__main__.py
@@ -10,41 +10,41 @@ parser = argparse.ArgumentParser()
 parser.add_argument("utility", help="name of utility to be run", type=str)
 parser.add_argument("domain", help="name of AWS domain", type=str)
 parser.add_argument("environment", help="name of environment", type=str, choices=["dev", "prd"])
-parser.add_argument("data_product", help="name of data product", type=str)
+parser.add_argument("instance", help="name/id of instance e.g. data product, service principal, etc.", type=str)
 parser.add_argument("other_argument", nargs="?", default="", help="others: table name, sql statement, etc.", type=str)
 args = parser.parse_args()
 utility = args.utility
 domain = args.domain
 environment = args.environment
-data_product = args.data_product
-other_argument = args.other_argument
 
 # Call utility
 match utility:
     case "dummy":
+        data_product = args.instance
         test_connect.ExampleSQLConnection(domain, environment, data_product)
     case "get_table_permissions":
-        if other_argument != "":
-            get_table_grant.GetTableGrant(domain, environment, data_product, other_argument)
-        else:
-            print("Undefined table name.")
+        data_product = args.instance
+        table_name = args.other_argument
+        get_table_grant.GetTableGrant(domain, environment, data_product, table_name)
     case "create_table":
-        if other_argument != "":
-            create_table.CreateTable(domain, environment, data_product, other_argument)
-        else:
-            print("Undefined table name.")
+        data_product = args.instance
+        table_name = args.other_argument
+        create_table.CreateTable(domain, environment, data_product, table_name)
     case "delete_table":
-        if other_argument != "":
-            delete_table.DeleteTable(domain, environment, data_product, other_argument)
-        else:
-            print("Undefined table name.")
+        data_product = args.instance
+        table_name = args.other_argument
+        delete_table.DeleteTable(domain, environment, data_product, table_name)
     case "get_sp_id":
+        data_product = args.instance
         get_service_principal_id.GetSpId(environment, data_product)
     case "get_sp_name":
-        service_principal_id_to_name.GetSpName(environment, data_product)
+        service_principal_id = args.instance
+        service_principal_id_to_name.GetSpName(environment, service_principal_id)
     case "sql_statement":
-        if other_argument != "":
-            sql.SqlByDataProductSp(domain, environment, data_product, other_argument)
+        data_product = args.instance
+        sql_statement = args.other_argument
+        if sql_statement != "":
+            sql.SqlByDataProductSp(domain, environment, data_product, sql_statement)
         else:
             print("Undefined SQL statement.")
     case _:
